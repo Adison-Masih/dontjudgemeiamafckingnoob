6d2e176b91d62d43fb7f2af995b42d9e878bcfc2
Adison Masih <68416649+Adison-Masih@users.noreply.github.com>
2021-01-06 08:45:32 +0530

    Create noobcode.js

---
+++ noobcode.js
@@ -0,0 +1,36 @@
+// disclaimer: i'm a noob you never know i shit my pants
+window.onPPStand = (metres) => {
+	function dontGiveaError(shit){
+		shit();
+	}
+
+	let letters = {
+		h : "h",
+		e : "e",
+		l: "l",
+		o: 'o',
+		w: 'w',
+		r: 'r',
+		d: 'd'
+	}
+
+	let h = "h";
+	h = "NOOBS DONT KNOW ALPHABETS!!!!";
+	h = letters.h; // amm i think this is right
+	let e = letters.e;
+	let l = letters.l;
+	let o = letters.o;
+	let w = letters.w;
+	let r = letters.r;
+	let d = letters.d;
+
+	function print_shit(h,e,l,o,w,r,d){
+		document.write(`${h}${e}${l}${l}${o} ${w}${o}${r}${l}${d}`);
+	}
+
+	dontGiveaError(print_shit(h,e,l,o,w,r,d));
+
+
+}
+
+window.onload = window.onPPStand(11000000000);
